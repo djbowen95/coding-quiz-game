da3170d0a069cc462f47a68108793601e7198edc
Daniel Bowen <djbowen95@gmail.com>
2022-01-10 18:33:18 +0000

    create index.html, style.css, script.js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// Code for a "Start" button, that disappears when quiz starts.
+
+// Code for a question, that is replaced with another question when answered.
+// - Objects/classes that contain the questions(?)
+// - Shuffle / randomly order the responses to present on the page(?).
+// - 
+
+// Code for a countdown timer.
+
+// Code which changes the timer when a question is answered wrong.
+
+// Code to keep track of a score.
+
+// Code to store a score. 
+
+// Code that ends the game when the timer reaches zero.
