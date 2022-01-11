39e9a5ac476b821671ae7b77a63028461f4ca4ab
Daniel Bowen <djbowen95@gmail.com>
2022-01-11 16:04:05 +0000

    fix object syntax bug

--- assets/script.js
+++ assets/script.js
@@ -73,51 +73,51 @@ function endGameDisplay () {
 // List of questions, correct answers, and wrong answers.
 // All 'answer1' must be true.
 const question1 = { 
-    question: "What is a boolean?";
-    answer1: "A data type that is either true or false.";
-    answer2: "A type of error, caused by ghosts.";
-    answer3: "A string that contains both letter and number characters.";
-    answer4: "A type of shoe traditionally worn by small Norwegian men.";
+    question: "What is a boolean?",
+    answer1: "A data type that is either true or false.",
+    answer2: "A type of error, caused by ghosts.",
+    answer3: "A string that contains both letter and number characters.",
+    answer4: "A type of shoe traditionally worn by small Norwegian men.",
 };
 
 const question2 = { 
-    question: "What is a string?";
-    answer1: "Zero or more characters written with quotes, which can be stored in a variable.";
-    answer2: "Several different variables, connected together in a row, like knots in a rope.";
-    answer3: "Something used to lace up traditional shoes worn by small Norwegian men.";
-    answer4: "A mathematical sum that is automatically calculated in the console.";
+    question: "What is a string?",
+    answer1: "Zero or more characters written with quotes, which can be stored in a variable.",
+    answer2: "Several different variables, connected together in a row, like knots in a rope.",
+    answer3: "Something used to lace up traditional shoes worn by small Norwegian men.",
+    answer4: "A mathematical sum that is automatically calculated in the console.",
 };
 
 const question3 = { 
-    question: "What command would you need to put in your code to display something in the console?";
-    answer1: "console.log('Your Text Here')";
-    answer2: "log.this('Your Text Here')";
-    answer3: "speak('Your Text Here')";
-    answer4: "help me little norwegian man, write this text('Your Text Here')";
+    question: "What command would you need to put in your code to display something in the console?",
+    answer1: "console.log('Your Text Here')",
+    answer2: "log.this('Your Text Here')",
+    answer3: "speak('Your Text Here')",
+    answer4: "help me little norwegian man, write this text('Your Text Here')",
 };
 
 const question4 = {
-    question: "Which of these could you use to start a conditional statement?";
-    answer1: "if";
-    answer2: "can";
-    answer3: "conditional-statement";
-    answer4: "clip-clap.the-magic-shoes";
+    question: "Which of these could you use to start a conditional statement?",
+    answer1: "if",
+    answer2: "can",
+    answer3: "conditional-statement",
+    answer4: "clip-clap.the-magic-shoes",
 };
 
 const question5 = {
-    question: "Why would you want to create a loop?";
-    answer1: "To repeat several lines of code multiple times.";
-    answer2: "To disrupt the time space continuum."
-    answer3: "To lace up your traditional wooden shoes."
-    answer4: "Its fun to annoy the computer."
+    question: "Why would you want to create a loop?",
+    answer1: "To repeat several lines of code multiple times.",
+    answer2: "To disrupt the time space continuum.",
+    answer3: "To lace up your traditional wooden shoes.",
+    answer4: "Its fun to annoy the computer.",
 }
 
 /*
 const question1 = { 
-    question: ;
-    answer1: ;
-    answer2: ;
-    answer3: ;
-    answer4: ;
+    question: ,
+    answer1: ,
+    answer2: ,
+    answer3: ,
+    answer4: ,
 };
 */
