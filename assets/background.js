ff8bb87722427c7f0c020baf39366df6c942a789
Daniel Bowen <djbowen95@gmail.com>
2022-11-10 16:54:04 +0000

    Bring granim script into HTML

--- assets/background.js
+++ assets/background.js
@@ -1,1 +1,5 @@
-console.log("Successfully linked");
+// For now, I'm going  to bring in the scripts seperately into the global score.
+// Will change when I bring in Handlebars or React.
+// https://stackoverflow.com/questions/9901082/what-is-this-javascript-require
+// const Granim = require("../node_modules/granim/dist/granim");
+
