567f4cb9847cef0e3d42a4db88f0fe534329d356
Daniel Bowen <djbowen95@gmail.com>
2022-11-10 17:06:26 +0000

    Set background to granim animated gradient

--- assets/background.js
+++ assets/background.js
@@ -3,3 +3,17 @@
 // https://stackoverflow.com/questions/9901082/what-is-this-javascript-require
 // const Granim = require("../node_modules/granim/dist/granim");
 
+const granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
